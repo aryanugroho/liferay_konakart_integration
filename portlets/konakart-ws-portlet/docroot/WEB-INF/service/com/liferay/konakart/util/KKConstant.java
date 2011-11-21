23d6681e61eb3bd352813d74511c926dad4a8673
Terry Jia <terry.jia@liferay.com>
2011-11-21 17:01:58 +0800

    rename the constants of konakart

--- portlets/konakart-ws-portlet/docroot/WEB-INF/service/com/liferay/konakart/util/KKConstant.java
+++ portlets/konakart-ws-portlet/docroot/WEB-INF/service/com/liferay/konakart/util/KKConstant.java
@@ -1,6 +1,6 @@
 package com.liferay.konakart.util;
 
-public interface KK_Constant {
+public interface KKConstant {
 	public int KK_DEFAULT_LANGUAGE_ID = -1;
 	
 	public int KK_NOT_CONSTRAINT_CATEGORY_ID = 0;
