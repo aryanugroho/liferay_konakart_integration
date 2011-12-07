301ded546b1f710527f9e676d89a882419fb22b0
Terry Jia <terry.jia@liferay.com>
2011-12-07 14:23:45 +0800

    rename the phase 1 portlet and add the phase's portlet "Detail portlet"

---
+++ portlets/konakart-ws-portlet/docroot/WEB-INF/src/com/liferay/konakart/DetailPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.konakart;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DetailPortlet
+ */
+public class DetailPortlet extends MVCPortlet {
+ 
+
+}
