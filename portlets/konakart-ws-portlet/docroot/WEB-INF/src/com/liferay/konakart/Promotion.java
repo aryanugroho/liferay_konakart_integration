30d42533cdbd390b284da178663e04887e6ade45
Terry Jia <terry.jia@liferay.com>
2012-01-03 10:16:56 +0800

    project's base

---
+++ portlets/konakart-ws-portlet/docroot/WEB-INF/src/com/liferay/konakart/Promotion.java
@@ -0,0 +1,11 @@
+package com.liferay.konakart;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class Promotion
+ */
+public class Promotion extends MVCPortlet {
+ 
+
+}
